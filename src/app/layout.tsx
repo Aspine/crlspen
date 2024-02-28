dcc8cf20d6d2a4d90886cc67166893ce40b392d6
ckuzmick <conrad.kuzmick@icloud.com>
2024-02-28 18:14:19 -0500

    stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./css/globals.css";
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
