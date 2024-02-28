3d7270f8bc9deee2b1f4d37429077317550b522e
Conrad Kuzmick <conrad.kuzmick@icloud.com>
2024-02-28 11:42:01 -0500

    fix logout page

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,10 +1,9 @@
-"use client";
-
 import React from "react";
 import NavBar from "@/components/navBar";
+import { cookies } from "next/headers";
 
 export default function Logout() {
-    document.cookie = "classData" + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/"; // removes the classData cookie, so logs you out :/
+    cookies().set("classData", "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/"); // removes the classData cookie, so logs you out :/
     window.location.reload(); // reloads the page just to make sure the cookie gets cleared
     window.location.href = "/"; // redirects back to the base page so that dashboard wont throw an error
     return (
