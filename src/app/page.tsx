ce32e85f4ef65a236d2b174348bc71b8d78823a2
Conrad Kuzmick <conrad.kuzmick@icloud.com>
2024-03-25 09:18:46 -0400

    redirect fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import falconImage from "@/../public/falcon.png";
-import redirect from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/login");
