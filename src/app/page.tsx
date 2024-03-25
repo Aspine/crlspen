4be7890102b647a8f715ceaa0f3907977e719f4e
Conrad Kuzmick <conrad.kuzmick@icloud.com>
2024-03-25 09:16:39 -0400

    redirect test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import React, { useState } from "react";
 import Image from "next/image";
 import falconImage from "@/../public/falcon.png";
+import redirect from "next/navigation";
 
 export default function Home() {
-  router.push("/login");
-  
+  redirect("/login");
+
   return <div></div>;
 }
