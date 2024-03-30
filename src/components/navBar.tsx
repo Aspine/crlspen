97207da3687d1d2d53d3619a3423410077fa2e33
eatingno0dles <161661029+eatingno0dles@users.noreply.github.com>
2024-03-30 17:32:52 -0400

    deleted a few things

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import daysLeft from "@/utils/daysLeft";
 
 export default function NavBar() {
   const days = daysLeft();
@@ -7,9 +6,6 @@ export default function NavBar() {
   return (
     <div className="navbar">
       <a href="/gradebook" className="title-link"><h1>CRLSpen</h1></a>
-      {/* <div className="daysLeft">
-        <p>{days} days left</p>
-      </div> */}
       <div className="navlinks">
         <a href="/gradebook">Grades</a>
         <a href="/events">Events</a>
