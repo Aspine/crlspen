2f07b2ccf726bf3f18c051e3cdc155cd87730b98
Conrad Kuzmick <conrad.kuzmick@icloud.com>
2024-05-16 10:20:01 -0400

    remove speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "CRLSpen",
@@ -16,7 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <body suppressHydrationWarning={true} >
         {children}
-        <SpeedInsights />
       </body>
     </html>
   );
